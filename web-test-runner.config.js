b716811ecc406a71678e7e7421f5da1ff08d6ee3
Remco Vaes <remcovaes@topdesk.com>
2022-08-23 09:34:14 +0200

    Add commonjs

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,13 +1,16 @@
 import rollupReplace from '@rollup/plugin-replace';
+import rollupCommonjs from '@rollup/plugin-commonjs';
 
 import { playwrightLauncher } from '@web/test-runner-playwright';
 import { fromRollup } from '@web/dev-server-rollup';
 
 const replace = fromRollup(rollupReplace);
+const commonjs = fromRollup(rollupCommonjs);
 
 export default {
 	nodeResolve: true,
 	plugins: [
+		commonjs(),
 		replace({ 'process.env.NODE_ENV': process.env.NODE_ENV, preventAssignment: true }),
 	],
 	browsers: [
